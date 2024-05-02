112200fb2b00675d0ff977068cee5e1bd9d6ee63
DylanBarratt <dylanbarratt03@gmail.com>
2024-05-02 19:19:09 +0100

    Movement switched to the silly one

--- Assets/Scripts/PlayerScripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerScripts/PlayerMovement.cs
@@ -1,62 +1,194 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour {
-    private Rigidbody2D rb;
-    private Animator anime;
+//I hate this code... was much nicer before but has been changed for assignment
 
-    private float speed = 4f, horizontal, vertical;
+public interface IPlayerState {
+    public IPlayerState Tick(GameObject player, PlayerInput input);
+    public void Enter(GameObject player, float speed);
+    public void Exit(GameObject player);
+}
 
-    private bool canHorizontal, canVertical;
+public class PlayerInput {
+    public bool W, A, S, D;
 
-    private const float MOVE_LIMITER = 0.7f;
-    
-    private Vector2 START_POS = new Vector2(0f, -4.5f);
-    
-    private void Start() {
-        rb = GetComponent<Rigidbody2D>();
-        anime = GetComponent<Animator>();
-        
-        canHorizontal = true;
-        canVertical = true;
-        
-        ResetLoc();
+    public PlayerInput(bool W, bool A, bool S, bool D) {
+        this.W = W;
+        this.A = A;
+        this.S = S;
+        this.D = D;
+    } 
+}
+
+public class PlayerIdle : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am idle");
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
     }
 
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return new PlayerLDiag();
+        if (input.D && (input.W || input.S)) return new PlayerRDiag();
+        if (input.W || input.S) return new PlayerUpDown();
+        if (input.A) return new PlayerLeft();
+        if (input.D) return new PlayerRight();
+        
+        return null; //stay idle
+    }
+
+    public void Exit(GameObject player) {
+        
+    }
+}
+
+public class PlayerLeft : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am left");
+        player.GetComponent<Animator>().SetBool("leftGlide", true);
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2( -speed, 0);
+
+
+    }
+
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return new PlayerLDiag();
+        if (input.D && (input.W || input.S)) return new PlayerRDiag();
+        if (input.W || input.S) return new PlayerUpDown();
+        if (input.A) return null;
+        if (input.D) return new PlayerRight();
+
+        return new PlayerIdle(); 
+    }
+
+    public void Exit(GameObject player) {
+        player.GetComponent<Animator>().SetBool("leftGlide", false);
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
+
+public class PlayerRight : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am right");
+        player.GetComponent<Animator>().SetBool("rightGlide", true);        
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2( speed, 0);
+
+    }
+
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return new PlayerLDiag();
+        if (input.D && (input.W || input.S)) return new PlayerRDiag();
+        if (input.W || input.S) return new PlayerUpDown();
+        if (input.A) return new PlayerLeft();
+        if (input.D) return null;
+
+        return new PlayerIdle(); 
+    }
+
+    public void Exit(GameObject player) {
+        player.GetComponent<Animator>().SetBool("rightGlide", false);
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
+
+public class PlayerUpDown : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am upDown");
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2(0, Input.GetAxisRaw("Vertical") * speed);
+
+    }
+
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return new PlayerLDiag();
+        if (input.D && (input.W || input.S)) return new PlayerRDiag();
+        if (input.W || input.S) return null;
+        if (input.A) return new PlayerLeft();
+        if (input.D) return new PlayerRight();
+        
+        return new PlayerIdle(); 
+    }
+
+    public void Exit(GameObject player) {
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
+
+public class PlayerLDiag : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am diag");
+        float speedLimit = speed * 0.7f;
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2( -speedLimit, Input.GetAxisRaw("Vertical") * speedLimit);
+    }
+
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return null;
+        if (input.D && (input.W || input.S)) return new PlayerRDiag();
+        if (input.W || input.S) return new PlayerUpDown();
+        if (input.A) return new PlayerLeft();
+        if (input.D) return new PlayerRight();
+        
+        return new PlayerIdle(); 
+    }
+
+    public void Exit(GameObject player) {
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
+
+public class PlayerRDiag : IPlayerState {
+    public void Enter(GameObject player, float speed) {
+        Debug.Log("I am diag");
+        float speedLimit = speed * 0.7f;
+        player.GetComponent<Rigidbody2D>().velocity = new Vector2( speedLimit, Input.GetAxisRaw("Vertical") * speedLimit);
+    }
+
+    public IPlayerState Tick(GameObject player, PlayerInput input) {
+        if (input.A && (input.W || input.S)) return new PlayerLDiag();
+        if (input.D && (input.W || input.S)) return null;
+        if (input.W || input.S) return new PlayerUpDown();
+        if (input.A) return new PlayerLeft();
+        if (input.D) return new PlayerRight();
+        
+        return new PlayerIdle(); 
+    }
+
+    public void Exit(GameObject player) {
+        player.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+    }
+}
+
+public class PlayerMovement : MonoBehaviour {
+    private Vector2 START_POS = new Vector2(0f, -4.5f);
+    private IPlayerState currentState = new PlayerIdle();
+
+    private float speed = 4f;
+    
+    private void Start() {
+        currentState.Enter(this.gameObject, speed);
+    }
+    
     private void SetSpeed(float val) {
         speed = val;
     }
 
-    private void ToggleHorizontal(bool val) {
-        canHorizontal = val;
-    }
-
-    private void ToggleVertical(bool val) {
-        canVertical = val;
-    }
-    
     private void Update() {
-        if (canHorizontal) {
-            horizontal = Input.GetAxisRaw("Horizontal");
-            anime.SetFloat("xVel", horizontal);
-        }
-
-        if (canVertical) {
-            vertical = Input.GetAxisRaw("Vertical");
-            anime.SetFloat("yVel", vertical);
-        }
+        UpdateState(new PlayerInput(
+            Input.GetKey(KeyCode.W), 
+            Input.GetKey(KeyCode.A), 
+            Input.GetKey(KeyCode.S), 
+            Input.GetKey(KeyCode.D)
+            ));
     }
 
-    private void FixedUpdate() {
-        //excessively slows controller movement :/
-        if (horizontal != 0 && vertical != 0) { // Check for diagonal movement
-            // limit movement speed diagonally, so you move at 70% speed
-            horizontal *= MOVE_LIMITER;
-            vertical *= MOVE_LIMITER;
-        }
+    private void UpdateState(PlayerInput input) {
+        IPlayerState newState = currentState.Tick(this.gameObject, input);
 
-        rb.velocity = new Vector2(horizontal * speed, vertical * speed);
+        if (newState != null) {
+            currentState.Exit(this.gameObject);
+            currentState = newState;
+            newState.Enter(this.gameObject, speed);
+        }
     }
 
     private void ResetLoc() {
